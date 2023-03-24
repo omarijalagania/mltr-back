040b1badaafbe083f79b669d21a6be25a7c442b7
omari <omari.jalagania@gmail.com>
2023-03-24 20:11:19 +0400

    session fix  build

---
+++ build/helpers/helpers.js
@@ -0,0 +1,3 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.generateCode=void 0;var _crypto=_interopRequireDefault(require("crypto"));function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}const generateCode=()=>{const str=Math.random().toString(36).substring(2,8);// Generate a random string
+return hashString(str).substring(0,6);// Hash the string and return the first 6 characters
+};exports.generateCode=generateCode;const hashString=(str,type="md5")=>{return _crypto.default.createHash(type).update(str).digest("hex")};
