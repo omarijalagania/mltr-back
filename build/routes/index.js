1efcd2dc1aca80b3e9ff97586ffef3baaa5d4453
omari <omari.jalagania@gmail.com>
2023-03-25 22:54:25 +0400

    chore: react build

--- build/routes/index.js
+++ build/routes/index.js
@@ -1,13 +1,1 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var _express = _interopRequireDefault(require("express"));
-var _auth = _interopRequireDefault(require("./auth"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const router = _express.default.Router();
-router.use(_auth.default);
-var _default = router;
-exports.default = _default;
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;var _express=_interopRequireDefault(require("express"));var _auth=_interopRequireDefault(require("./auth"));function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}const router=_express.default.Router();router.use(_auth.default);var _default=router;exports.default=_default;
