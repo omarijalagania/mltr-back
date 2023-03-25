5fe7506bc2d3161c0f451e8860271d1018b3b216
omari <omari.jalagania@gmail.com>
2023-03-25 22:34:55 +0400

    chore: react build

--- src/app.ts
+++ src/app.ts
@@ -20,12 +20,14 @@ connectDB(false)
     app.use(bodyParser.json())
     app.use(
       cors({
-        origin: ["https://auth-react.onrender.com", "http://localhost:3000"],
+        origin: true,
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         optionsSuccessStatus: 200,
       }),
     )
+
+    //["https://auth-react.onrender.com", "http://localhost:3000"]
 
     const sessionConfig = {
       secret: "mysecretsdfsdfkljsadflksjflsjkdflkj",
