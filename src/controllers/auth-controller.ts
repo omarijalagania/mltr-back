9030898ce5ce80a49ed693df22cf93db84dd788c
omari <omari.jalagania@gmail.com>
2023-03-25 21:23:55 +0400

    chore: react build

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -31,7 +31,7 @@ export const userLogin = async (
       if (!user) {
         return res.status(401).json({ message: info.message })
       }
-      req.logIn(user, function (err) {
+      req.login(user, function (err) {
         if (err) {
           return next(err)
         }
