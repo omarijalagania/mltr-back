c044a9f7d6c5429e88ae96696f3e04a6abd615a0
omari <omari.jalagania@gmail.com>
2023-03-27 22:07:27 +0400

    wip: dashboar

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ connectDB(false)
     app.use(bodyParser.json())
     app.use(
       cors({
-        origin: true,
+        origin: "http://localhost:3000",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         optionsSuccessStatus: 200,
