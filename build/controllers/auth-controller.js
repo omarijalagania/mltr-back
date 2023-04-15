49d476d628edaa82ca66e75a27727551715b0b26
omari <omari.jalagania@gmail.com>
2023-04-15 21:26:03 +0400

    fix: add documentation

--- build/controllers/auth-controller.js
+++ build/controllers/auth-controller.js
@@ -32,6 +32,7 @@ const registerWithGoogle = async (req, res) => {
         message: "User with this email already exists"
       });
     } else {
+      var _user, _user2;
       user = await _models.User.create({
         email: login,
         sex,
@@ -43,13 +44,13 @@ const registerWithGoogle = async (req, res) => {
         weight,
         is_ft_weight
       });
-      /*     const token = jwt.sign(
-        { _id: user?._id, name: user?.email },
-        process.env.JWT_SECRET
-      ) */
+      const token = _jsonwebtoken.default.sign({
+        _id: (_user = user) === null || _user === void 0 ? void 0 : _user._id,
+        name: (_user2 = user) === null || _user2 === void 0 ? void 0 : _user2.email
+      }, process.env.JWT_SECRET);
       return res.status(201).json({
-        message: "User Registered."
-        /*   token,
+        message: "User Registered.",
+        token,
         _id: user._id,
         email: user.email,
         sex: user.sex,
@@ -59,7 +60,7 @@ const registerWithGoogle = async (req, res) => {
         body_type: user.body_type,
         physical_activities: user.physical_activities,
         weight: user.weight,
-        is_ft_weight: user.is_ft_weight, */
+        is_ft_weight: user.is_ft_weight
       });
     }
   } catch (error) {
@@ -124,7 +125,7 @@ const registerWithApple = async (req, res) => {
       appleToken
     });
     if (user) {
-      var _user, _user2, _user3;
+      var _user3, _user4, _user5;
       // User already exists, update user with new data
       user = await _models.User.findOneAndUpdate({
         appleToken
@@ -143,16 +144,16 @@ const registerWithApple = async (req, res) => {
         new: true
       });
       const token = _jsonwebtoken.default.sign({
-        _id: (_user = user) === null || _user === void 0 ? void 0 : _user._id,
-        name: (_user2 = user) === null || _user2 === void 0 ? void 0 : _user2.email,
-        appleToken: (_user3 = user) === null || _user3 === void 0 ? void 0 : _user3.appleToken
+        _id: (_user3 = user) === null || _user3 === void 0 ? void 0 : _user3._id,
+        name: (_user4 = user) === null || _user4 === void 0 ? void 0 : _user4.email,
+        appleToken: (_user5 = user) === null || _user5 === void 0 ? void 0 : _user5.appleToken
       }, process.env.JWT_SECRET);
       return res.status(201).json({
         message: "User Updated and logged in.",
         token
       });
     } else {
-      var _user4, _user5, _user6;
+      var _user6, _user7, _user8;
       user = await _models.User.create({
         email: login,
         appleToken,
@@ -166,9 +167,9 @@ const registerWithApple = async (req, res) => {
         is_ft_weight
       });
       const token = _jsonwebtoken.default.sign({
-        _id: (_user4 = user) === null || _user4 === void 0 ? void 0 : _user4._id,
-        name: (_user5 = user) === null || _user5 === void 0 ? void 0 : _user5.email,
-        appleToken: (_user6 = user) === null || _user6 === void 0 ? void 0 : _user6.appleToken
+        _id: (_user6 = user) === null || _user6 === void 0 ? void 0 : _user6._id,
+        name: (_user7 = user) === null || _user7 === void 0 ? void 0 : _user7.email,
+        appleToken: (_user8 = user) === null || _user8 === void 0 ? void 0 : _user8.appleToken
       }, process.env.JWT_SECRET);
       return res.status(201).json({
         message: "User Registered and logged in.",
@@ -219,6 +220,11 @@ const loginWithApple = async (req, res) => {
         physical_activities: user.physical_activities,
         weight: user.weight,
         is_ft_weight: user.is_ft_weight
+      });
+    }
+    if (!user) {
+      res.status(404).json({
+        message: "User not found"
       });
     }
   } catch (error) {
