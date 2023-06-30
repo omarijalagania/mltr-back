93c9083c1e1df42b9e09117c580eeefb4e7d24d1
omari <omari.jalagania@gmail.com>
2023-06-30 14:44:37 +0400

    style: fixing colors 2

--- src/mail/template.ts
+++ src/mail/template.ts
@@ -29,17 +29,17 @@ export const codeConfirmationTemplate = (code: string) => {
     <div style="padding: 10px 20px;">
       <img src="https://mltr.ibotchori.space/images/mltr-logo.png" alt="MLTR Logo" style="max-width: 100%; width: 85px; height: 85px;">
       
-      <p style="@media (prefers-color-scheme: dark) { color: #FFFFFF; !important; }">Hello there, Thank you for registering with MLTR!</p> 
-      <p style="@media (prefers-color-scheme: dark) { color: #FFFFFF; !important; }">We're so happy you're here and can't wait for you to start using our app to track your nutrition. To complete the registration process, please enter the following verification code in the app:</p>
+      <p style="@media (prefers-color-scheme: dark) { color: #FFFFFF !important; }">Hello there, Thank you for registering with MLTR!</p> 
+      <p style="@media (prefers-color-scheme: dark) { color: #FFFFFF !important; }">We're so happy you're here and can't wait for you to start using our app to track your nutrition. To complete the registration process, please enter the following verification code in the app:</p>
       
-      <h1 style="@media (prefers-color-scheme: dark) { color: #C4FF46; !important; }; font-size: 34px; margin: 10px 0;">${code}</h1>
+      <h1 style="@media (prefers-color-scheme: dark) { color: #C4FF46 !important; }; font-size: 34px; margin: 10px 0;">${code}</h1>
       
-      <p style="@media (prefers-color-scheme: dark) { color: #FFFFFF; !important; }">
+      <p style="@media (prefers-color-scheme: dark) { color: #FFFFFF !important; }">
         This will verify your email and allow you to fully access all the features of the app. If you did not request this verification code, please disregard this email. If you have any questions or concerns, please contact our support team at
         <a href="mailto:mltr.support@onyxlabs.tech" style="color: #c4ff46; text-decoration: none;">mltr.support@onyxlabs.tech</a>.
       </p>
       
-      <p style="@media (prefers-color-scheme: dark) { color: #FFFFFF; !important; }">
+      <p style="@media (prefers-color-scheme: dark) { color: #FFFFFF !important; }">
         If you have any questions or concerns about the verification process, please don't hesitate to contact our support team. We're always here to help. Thanks for choosing our app to help you reach your nutrition goals!
         <br><br>
         Warm regards,<br>
@@ -49,9 +49,9 @@ export const codeConfirmationTemplate = (code: string) => {
       <hr style="width: 100%; height: 1px; background-color: #797979 !important; margin: 10px 0; border: none;">
       <div style="display: flex; align-items: center; justify-content: center;">
         <img src="https://mltr.ibotchori.space/images/onyx-logo.png" alt="ONYX Labs Logo" style="width: 36px; height: 36px; display: block;">
-        <div style="font-size: 14px; text-align: center; margin-left: 5px; @media (prefers-color-scheme: dark) { color: #FFFFFF; !important; }">ONYX Labs</div>
-        <a href="https://onyxlabs.tech/terms.html" style="font-size: 14px; @media (prefers-color-scheme: dark) { color: #C4FF46; !important; }; margin-left: 5px; text-decoration: none;">Terms and Conditions</a>
-        <a href="https://onyxlabs.tech/privacy.html" style="font-size: 14px; @media (prefers-color-scheme: dark) { color: #C4FF46; !important; }; margin-left: 5px; text-decoration: none; margin-left: 10px;">Privacy Policy</a>
+        <div style="font-size: 14px; text-align: center; margin-left: 5px; @media (prefers-color-scheme: dark) { color: #FFFFFF !important; }">ONYX Labs</div>
+        <a href="https://onyxlabs.tech/terms.html" style="font-size: 14px; @media (prefers-color-scheme: dark) { color: #C4FF46 !important; }; margin-left: 5px; text-decoration: none;">Terms and Conditions</a>
+        <a href="https://onyxlabs.tech/privacy.html" style="font-size: 14px; @media (prefers-color-scheme: dark) { color: #C4FF46 !important; }; margin-left: 5px; text-decoration: none; margin-left: 10px;">Privacy Policy</a>
       </div>
     </div>
     
