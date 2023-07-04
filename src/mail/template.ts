8e0a36b62dbebce60f356b9d6ca84adfdf2ef14a
omari <omari.jalagania@gmail.com>
2023-07-04 12:42:05 +0400

    fix: channge style

--- src/mail/template.ts
+++ src/mail/template.ts
@@ -10,19 +10,21 @@ export const codeConfirmationTemplate = (code: string) => {
 </head>
 
 <body style="margin: 0; display: flex; justify-content: center; align-items: center; flex-direction: column; width: 100%; height: 100%; padding: 0;">
-  <div style="background-image: url('https://mltr.ibotchori.space/images/tile.png');  font-weight: 400; text-align: center; height: 100%; display: flex; align-items: center; justify-content: center;">
+  <div style="background-image: url('https://mltr.ibotchori.space/images/tile.png');  font-size: 16px; font-weight: 400; text-align: center; height: 100%; display: flex; align-items: center; justify-content: center;">
     <div style="padding: 10px 20px;">
-      <p style="">Hello there, Thank you for registering with MLTR!</p> 
-      <p style="">We're so happy you're here and can't wait for you to start using our app to track your nutrition. To complete the registration process, please enter the following verification code in the app:</p>
+      <img src="https://mltr.ibotchori.space/images/mltr-logo.png" alt="MLTR Logo" style="max-width: 100%; width: 85px; height: 85px;">
       
-      <h1 style="color: #A8E717; font-size: 34px; margin: 10px 0;">${code}</h1>
+      <p>Hello there, Thank you for registering with MLTR!</p> 
+      <p>We're so happy you're here and can't wait for you to start using our app to track your nutrition. To complete the registration process, please enter the following verification code in the app:</p>
       
-      <p style="">
+      <h1 style="color: #c4ff46; font-size: 34px; margin: 10px 0;">${code}</h1>
+      
+      <p>
         This will verify your email and allow you to fully access all the features of the app. If you did not request this verification code, please disregard this email. If you have any questions or concerns, please contact our support team at
-        <a href="mailto:mltr.support@onyxlabs.tech" style="color: #A8E717; text-decoration: none;">mltr.support@onyxlabs.tech</a>.
+        <a href="mailto:mltr.support@onyxlabs.tech" style="color: #c4ff46; text-decoration: none;">mltr.support@onyxlabs.tech</a>.
       </p>
       
-      <p style="">
+      <p>
         If you have any questions or concerns about the verification process, please don't hesitate to contact our support team. We're always here to help. Thanks for choosing our app to help you reach your nutrition goals!
         <br><br>
         Warm regards,<br>
@@ -31,9 +33,10 @@ export const codeConfirmationTemplate = (code: string) => {
       
       <hr style="width: 100%; height: 1px; background-color: #797979; margin: 10px 0; border: none;">
       <div style="display: flex; align-items: center; justify-content: center;">
+        <img src="https://mltr.ibotchori.space/images/onyx-logo.png" alt="ONYX Labs Logo" style="width: 36px; height: 36px; display: block;">
         <div style="font-size: 14px; text-align: center; margin-left: 5px; ">ONYX Labs</div>
-        <a href="https://onyxlabs.tech/terms.html" style="font-size: 14px;  color: #A8E717; margin-left: 5px; text-decoration: none;">Terms and Conditions</a>
-        <a href="https://onyxlabs.tech/privacy.html" style="font-size: 14px; color: #A8E717; margin-left: 5px; text-decoration: none; margin-left: 10px;">Privacy Policy</a>
+        <a href="https://onyxlabs.tech/terms.html" style="font-size: 14px;  color: #c4ff46; margin-left: 5px; text-decoration: none;">Terms and Conditions</a>
+        <a href="https://onyxlabs.tech/privacy.html" style="font-size: 14px; color: #c4ff46; margin-left: 5px; text-decoration: none; margin-left: 10px;">Privacy Policy</a>
       </div>
     </div>
     
