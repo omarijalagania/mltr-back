6caea106d7dcb13df4365b5dbc2380b087b1d9f5
root <omar.jalagania@gmail.com>
2024-10-08 18:50:19 +0000

    ch

---
+++ build/routes/auth.js
@@ -0,0 +1,7 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;var _express=_interopRequireDefault(require("express"));var _controllers=require("../controllers");var _middlewares=require("../middlewares");function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}const router=_express.default.Router();// Google - auth route
+router.post("/google-register",_controllers.registerWithGoogle);router.post("/google-login",_controllers.loginWithGoogle);// Apple - auth route
+router.post("/apple-register",_controllers.registerWithApple);router.post("/apple-login",_controllers.loginWithApple);// Email & Password
+router.post("/login",_controllers.userLogin);router.post("/register",_controllers.userRegister);router.post("/get-code",_controllers.getConfirmationCode);router.post("/deactivate-account",_middlewares.authMiddleware,_controllers.deactivateAccount);router.post("/confirm-deactivate-account",_middlewares.authMiddleware,_controllers.confirmDeactivationCode);router.patch("/update-user",_middlewares.authMiddleware,_controllers.updateUser);router.get("/get-user",_middlewares.authMiddleware,_controllers.getUser);router.get("/user-details/:userId",_middlewares.authMiddleware,_controllers.getUserDetails);//Pro
+router.post("/pro",_middlewares.authMiddleware,_controllers.userBuyPro);//Token verify
+router.get("/verify",_controllers.tokenVerify);//Admin routes
+router.get("/admin/users",_middlewares.authMiddleware,_controllers.getAllUsers);var _default=router;exports.default=_default;
