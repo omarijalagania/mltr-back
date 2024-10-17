6bd98706589600927d396937cbfadb5f6991406b
Omari <omari.jalagania@gmail.com>
2024-10-17 14:20:05 +0400

    wip: change cors

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,12 @@ connectDB(false)
     app.use(express.json({ limit: "50mb" }))
     app.use(express.urlencoded({ limit: "50mb" }))
 
-    app.use(cors({}))
+    app.use(
+      cors({
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      }),
+    )
 
     // app.get("/", async (_, res) => {
     //   res.status(200).send("Welcome to Node.js Server")
