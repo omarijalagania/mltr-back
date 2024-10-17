8045f6d9980d11e618e9aa04615d18ea214290b3
root <omar.jalagania@gmail.com>
2024-10-17 10:21:56 +0000

    cor

--- build/mail/sorry.js
+++ build/mail/sorry.js
@@ -12,6 +12,7 @@
     <style>
       /* Inline styles for maximum compatibility */
       body {
+        
         padding: 15px;
       }
 
@@ -43,45 +44,61 @@
         h1 {
           color: #8be100 !important;
         }
+        hr {
+            background-color: #858A97 !important;
+            }
       }
     </style>
 
+    <style>
+    .onyx-logo { margin-top: 8px !important; }
+    @media screen and (max-width: 450px) 
+    { 
+    
+    .content { width: 100% !important; display: block !important; }
+    
+    } 
+</style>
 
   </head>
-  <body data-ogsc="light-mode">
-    <img src="${process.env.BACKEND_URL}/images/mltr.png" alt="mltr" />
+  <body class="body-content" style="max-width: 450px !important; margin: 0 auto !important;" data-ogsc="light-mode">
+    <img width="85px" height="85px" src="${process.env.BACKEND_URL}/images/MLTR_logo_500.png" alt="mltr" />
     <h1>
       We are sorry to see you go
     </h1>
 
-    <p>
+    <p style="
+          font-weight: 400;
+          font-size: 16px;
+          line-height: 22px;
+          ">
       Thank you for giving MLTR a chance. We're sorry to see you go and would
       love to know how we could have made your experience better. If you have
       any feedback or suggestions, please consider sharing them with our
       development team at
       <span
         style="
-          color: #c4ff46;
-          font-weight: 700;
+          color: #8be100;
+          font-weight: 400;
           font-size: 16px;
           line-height: 22px;
         "
-        >mltr.support@onyxlabs.tech</span
+        >mltr.suggestions@onyxlabs.tech</span
       >
       Your input is invaluable and can help us improve.
     </p>
 
-    <p style="text-align: left; font-size: 14px; line-height: 22px">
+    <p style="text-align: left; font-weight: 400; font-size: 16px; line-height: 22px">
       Please do not share this verification code with anyone. It’s our little
       secret.
     </p>
 
     <p style="font-weight: 400; font-size: 16px; line-height: 22px">
-      <span style="font-weight: 700">Country:</span> ${ip?ip:"-"}
+      <span style="font-weight: 700; font-size: 16px; line-height: 22px">Country:</span> ${ip?ip:"-"}
     </p>
 
     <p style="font-weight: 400; font-size: 16px; line-height: 22px">
-      <span style="font-weight: 700">Device:</span> ${device?device:"-"}
+      <span style="font-weight: 700; font-size: 16px; line-height: 22px">Device:</span> ${device?device:"-"}
     </p>
 
     <h1>
@@ -104,11 +121,11 @@
         team at
         <span
           style="
-            color: #c4ff46;
-            font-weight: 700;
-            font-size: 16px;
-            line-height: 22px;
-          "
+          color: #8be100;
+          font-weight: 400;
+          font-size: 16px;
+          line-height: 22px;
+        "
           >mltr.support@onyxlabs.tech</span
         >
       </p>
@@ -127,24 +144,24 @@
     </div>
 
     <!-- Footer -->
-    <hr style="margin-top: 60px" />
-      <table style="width: 95%; margin-top: 60px; padding: 10px 0; border-collapse: collapse;">
+    <hr style="margin-top: 60px; border: none !important; background-color: #858A97 !important; height: 0.5px !important;" />
+      <table class="table-width" style="width: 90%; margin-top: 60px; padding: 10px 0; border-collapse: collapse;">
       <tr>
-        <td style="padding-right: 5px;">
-          <img src="${process.env.BACKEND_URL}/images/onyx.png" alt="onyx" />
+        <td>
+          <img class="onyx-logo" width="25px" height="25px" src="${process.env.BACKEND_URL}/images/ONYX_logo_100.png" alt="onyx" />
         </td>
-        <td style="padding-right: 5px;">
-          <a style="color: #7c7c7c; font-weight: 400; font-size: 14px; line-height: 20px; text-decoration: none;" href="https://mltr.app/policies/terms.html">
+        <td>
+          <a style="color: #7c7c7c; font-weight: 400; font-size: 14px; line-height: 20px;" href="https://mltr.app/policies/terms.html">
             ONYX Labs
           </a>
         </td>
-        <td style="padding-right: 5px;">
-          <a style="color: #c4ff46; font-weight: 400; font-size: 14px; line-height: 20px; text-decoration: none;" href="https://mltr.app/policies/terms.html">
+        <td class="content">
+          <a style="color: #8be100; font-weight: 400; font-size: 14px; line-height: 20px;" href="https://mltr.app/policies/terms.html">
             Terms and Conditions
           </a>
         </td>
-        <td>
-          <a style="color: #c4ff46; font-weight: 400; font-size: 14px; line-height: 20px; text-decoration: none;" href="https://mltr.app/policies/privacy.html">
+        <td class="content">
+          <a style="color: #8be100; font-weight: 400; font-size: 14px; line-height: 20px;" href="https://mltr.app/policies/privacy.html">
             Privacy Policy
           </a>
         </td>
