df16b1dbe861e5699f976d28814df9924f23fd83
Omari <omari.jalagania@gmail.com>
2024-10-17 14:19:48 +0400

    wip: change cors

--- src/routes/history.ts
+++ src/routes/history.ts
@@ -26,16 +26,13 @@ router.delete("/remove", authMiddleware, deleteHistory)
 router.get("/all/:userId", authMiddleware, getAllHistory)
 router.post("/add-new-history", authMiddleware, addNewHistory)
 router.patch("/edit-new-history", authMiddleware, editNewHistory)
-<<<<<<< HEAD
 router.patch("/edit-new-history-part", authMiddleware, editNewHistoryParts)
 router.delete(
   "/delete-new-history-part",
   authMiddleware,
   deleteSpecificFoodFromHistory,
 )
-=======
 router.post("/edit-new-history-part", authMiddleware, editNewHistoryParts)
->>>>>>> master
 router.delete("/delete-new-history", authMiddleware, deleteNewHistory)
 router.post(
   "/delete-new-history-part",
