25b3254165f3e8bcab9be86814c68cbb533c5880
Omari <omari.jalagania@gmail.com>
2024-10-23 19:18:36 +0400

    wip: bug route

--- build/middlewares/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});Object.defineProperty(exports,"authMiddleware",{enumerable:true,get:function(){return _authMiddleware.default}});var _authMiddleware=_interopRequireDefault(require("./auth-middleware"));function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}
