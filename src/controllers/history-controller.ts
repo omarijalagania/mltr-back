b27223370f6a8b0fd84ddb03a6340033fec53c1f
Omari <omari.jalagania@gmail.com>
2024-10-25 12:24:58 +0400

    Refactor history-controller.ts for editing and deleting food from user's history

--- src/controllers/history-controller.ts
+++ src/controllers/history-controller.ts
@@ -1,6 +1,6 @@
 import e, { Request, Response } from "express"
 import { decodeTokenAndGetUserId, isValidId } from "helpers"
-import { History, UserFoodHistory, UserFoodList } from "models"
+import { History, UserFoodHistory } from "models"
 
 export const createHistory = async (req: Request, res: Response) => {
   const { userId, weight, date } = req.body
@@ -367,3 +367,75 @@ export const deleteSpecificFoodFromHistory = async (
     return res.status(500).json({ message: "Something went wrong..." })
   }
 }
+
+// export const editNewHistoryParts = async (req: Request, res: Response) => {
+//   const { userId, historyList, foodId } = req.body
+
+//   const isUserIdValid = decodeTokenAndGetUserId(req, userId)
+
+//   const updatedProperties = {} as { [key: string]: any }
+//   for (const key in historyList) {
+//     if (historyList.hasOwnProperty(key)) {
+//       updatedProperties[`userFoodHistoryList.$.${key}`] = historyList[key]
+//     }
+//   }
+
+//   try {
+//     if (!isUserIdValid) {
+//       return res.status(403).json({ message: "Not authorized" })
+//     }
+
+//     const updatedHistory = await UserFoodHistory.findOneAndUpdate(
+//       { userId: userId, "userFoodHistoryList._id": foodId },
+//       {
+//         $set: {
+//           ...updatedProperties,
+//         },
+//       },
+//       { new: true },
+//     )
+
+//     if (!updatedHistory) {
+//       return res.status(404).json({ message: "History not found" })
+//     }
+
+//     return res
+//       .status(200)
+//       .json({ message: "History edited", history: updatedHistory })
+//   } catch (error) {
+//     console.error("Error updating history:", error) // Debugging log
+//     return res.status(500).json({ message: "Something went wrong..." })
+//   }
+// }
+
+// export const deleteSpecificFoodFromHistory = async (
+//   req: Request,
+//   res: Response,
+// ) => {
+//   const { userId, foodId } = req.body
+
+//   const isUserIdValid = decodeTokenAndGetUserId(req, userId)
+
+//   try {
+//     if (!isUserIdValid) {
+//       return res.status(403).json({ message: "Not authorized" })
+//     }
+
+//     const updatedHistory = await UserFoodHistory.findOneAndUpdate(
+//       { userId: userId },
+//       { $pull: { userFoodHistoryList: { _id: foodId } } },
+//       { new: true },
+//     )
+
+//     if (!updatedHistory) {
+//       return res.status(404).json({ message: "History not found" })
+//     }
+
+//     return res
+//       .status(200)
+//       .json({ message: "Food deleted from history", history: updatedHistory })
+//   } catch (error) {
+//     console.error("Error deleting food from history:", error) // Debugging log
+//     return res.status(500).json({ message: "Something went wrong..." })
+//   }
+// }
